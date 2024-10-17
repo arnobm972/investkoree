d724806ea3e027871a3660a5b0bb3aa718e8b8f7
unknown <arnobm97@gmail.com>
2024-10-17 15:27:43 +0600

    sdf

--- client-side/vite.config.js
+++ client-side/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Specify the output directory
+    emptyOutDir: true, // Clear the output directory before each build
+  },
+
   server :{
     port:3000,
     proxy :{
