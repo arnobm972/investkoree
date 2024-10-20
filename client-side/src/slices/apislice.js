0ec120a55ac9b490af0080f82ec84a4a4dbfc068
unknown <arnobm97@gmail.com>
2024-10-20 11:46:25 +0600

    asdsd

--- client-side/src/slices/apislice.js
+++ client-side/src/slices/apislice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 // Base API slice configuration
 // const baseQuery = fetchBaseQuery({ baseUrl: 'investkoree-server-side.vercel.app' });
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://investkoree-server-side.vercel.app/api' });
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://investkoree-server-side.vercel.app' });
 
 export const apiSlice = createApi({
   baseQuery,
