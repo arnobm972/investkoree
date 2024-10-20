8a5b6603c603ffc4bc7cdfff15023028bf263597
unknown <arnobm97@gmail.com>
2024-10-20 13:52:18 +0600

    asdsd

--- client-side/tailwind.config.js
+++ client-side/tailwind.config.js
@@ -11,6 +11,14 @@ export default {
         salmon: '#FA8072', // Original color
         salmonLight: '#FFB3A1', // Lighter version
       },
+      screens: {
+        'xs': '480px', 
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
     },
   },
   plugins: [require("daisyui")],
