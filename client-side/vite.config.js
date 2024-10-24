fbac9e78ce8b15864ceb44788368bbff57bbd922
unknown <arnobm97@gmail.com>
2024-10-24 12:21:50 +0600

    backend added for vercel deployment

--- client-side/vite.config.js
+++ client-side/vite.config.js
@@ -22,9 +22,9 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': {
+      '/api/users': {
         // Use the correct target for production, you'll replace this with your Vercel deployment URL after deployment
-        target: isProduction ? 'https://investkoree-server-side.vercel.app/' : 'http://localhost:5000',
+        target: isProduction ? 'https://investkoree-server-side.vercel.app' : 'http://localhost:5000',
         changeOrigin: true,
       },
     },
