9f1b507b96acf54363c19113c4bb4865152fae3f
unknown <arnobm97@gmail.com>
2024-10-27 10:29:00 +0600

    dsd

--- client-side/vite.config.js
+++ client-side/vite.config.js
@@ -1,32 +1,39 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-const isProduction = process.env.NODE_ENV === 'production';
+// const isProduction = process.env.NODE_ENV === 'production';
 
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist',
+//     emptyOutDir: true,
+//     sourcemap: true, 
+//     rollupOptions: {
+//       output: {
+//         manualChunks: id => {
+//           if (id.includes('node_modules')) {
+//             return id.toString().split('node_modules/')[1].split('/')[0];
+//           }
+//         },
+//       },
+//     },
+//   },
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       '/api': {
+//         target: isProduction ? 'https://investkoree-server-side.vercel.app' : 'http://localhost:5000',
+//         changeOrigin: true,
+//         secure: false,  // Optional, helps with some proxy issues
+//       },
+//     },
+//   },
+// });
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: true, 
-    rollupOptions: {
-      output: {
-        manualChunks: id => {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0];
-          }
-        },
-      },
-    },
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: isProduction ? 'https://investkoree-server-side.vercel.app' : 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,  // Optional, helps with some proxy issues
-      },
-    },
-  },
 });
--- server-side/api/users/index.js
+++ server-side/api/users/index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from '../../config/db.js';
 import { notFound, errorHandler } from '../../middleware/errorMiddleware.js';
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
+// import cors from 'cors';
 import bodyParser from 'body-parser';
 import userRoutes from '../../routes/userRoutes.js'; 
 
@@ -14,36 +14,36 @@ const app = express();
 connectDB();
 
 // Define allowed origins
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://luminous-caramel-715e81.netlify.app',
-  // 'https://investkoree-server-side.vercel.app'
-];
+// const allowedOrigins = [
+//   'http://localhost:3000',
+//   'https://luminous-caramel-715e81.netlify.app',
+//   // 'https://investkoree-server-side.vercel.app'
+// ];
 
-// CORS options
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
+// // CORS options
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (allowedOrigins.includes(origin) || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true,
+//   optionsSuccessStatus: 200,
+// };
 
 
 
-app.use(cors(corsOptions)); 
+// app.use(cors(corsOptions)); 
 
 // Handle preflight requests
  
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
