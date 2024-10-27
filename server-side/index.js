6e66425aca420b6e91fb68de6b7d75e9eac1e248
unknown <arnobm97@gmail.com>
2024-10-27 12:08:14 +0600

    dsd

--- server-side/index.js
+++ server-side/index.js
@@ -61,11 +61,11 @@ app.use('/api/users', userRoutes);
 // app.use(notFound);
 // app.use(errorHandler);
 
-// if (process.env.NODE_ENV !== 'production') {
-//   const port = process.env.PORT || 5000;
-//   app.listen(port, () => {
-//     console.log(`Server started on port ${port}`);
-//   });
-// }
+if (process.env.NODE_ENV !== 'production') {
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
+}
 
 export default app;
