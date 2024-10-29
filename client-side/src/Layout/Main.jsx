93cc157007e8af855208e07c0bb3db26828c2710
unknown <arnobm97@gmail.com>
2024-10-29 10:52:04 +0600

    asdasd

--- client-side/src/Layout/Main.jsx
+++ client-side/src/Layout/Main.jsx
@@ -2,17 +2,14 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../shared/Navbar";
 import Footer from "../shared/Footer";
 import { ToastContainer } from "react-toastify";
-import AuthProvider from "../providers/AuthProvider";
 
 const Main = () => {
   return (
     <div className="roboto-regular">
-      <AuthProvider>
-        <Navbar></Navbar>
-        <ToastContainer></ToastContainer>
-        <Outlet></Outlet>
-        <Footer></Footer>
-      </AuthProvider>
+      <Navbar></Navbar>
+      <ToastContainer></ToastContainer>
+      <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
