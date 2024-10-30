f2a765455cdfb6e53d3e9fe627d8e8b6cff82e0e
unknown <arnobm97@gmail.com>
2024-10-30 11:35:54 +0600

    asdsdda

--- server-side/config/jwtConfig.js
+++ server-side/config/jwtConfig.js
@@ -2,8 +2,8 @@ import crypto from 'crypto'
 
 
 
-const secretkey =crypto.randomBytes(32).toString('hex');
+const secretKey =crypto.randomBytes(32).toString('hex');
 
 export default {
-    secretkey: secretkey
+    secretKey: secretKey
   };
