2a1c0fd3f4e8cfa13518e52d4d0f05e88b2fdb90
unknown <arnobm97@gmail.com>
2024-10-31 13:20:44 +0600

    asdasd

--- server-side/index.js
+++ server-side/index.js
@@ -51,7 +51,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use("/users", signupRoute);  
 app.use("/users/auth", loginRoute);
-app.use("/founder", founderPostRoute);
+app.use("/founderpost", founderPostRoute);
 
 // Routes
 app.use('/users', userRoutes);
