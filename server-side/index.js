1fb1e3ba25a344697aeced99f24487b6d8b4a814
unknown <arnobm97@gmail.com>
2024-11-03 13:21:22 +0600

    asdds

--- server-side/index.js
+++ server-side/index.js
@@ -49,7 +49,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Serve static files from the upload directory
-app.use('/upload',cpUpload, express.static(path.join(__dirname, 'upload')))
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
 
 // Route definitions
 app.use("/users", signupRoute);
