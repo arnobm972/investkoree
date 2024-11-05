bb81f7408d5ddaf0753bec5d0fa4e902f9976112
unknown <arnobm97@gmail.com>
2024-11-05 13:41:22 +0600

    asdasds

--- server-side/middleware/authMiddleware.js
+++ server-side/middleware/authMiddleware.js
@@ -1,29 +1,29 @@
-import admin from '../firebaseAdmin.js';
+// import admin from '../firebaseAdmin.js';
 
 
 
 
 
-const authMiddleware = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).json({ message: "No token provided" });
-  }
+// const authMiddleware = (req, res, next) => {
+//   const authHeader = req.headers.authorization;
+//   if (!authHeader) {
+//     return res.status(401).json({ message: "No token provided" });
+//   }
 
-  const token = authHeader.split(" ")[1];
-  if (!token) {
-    return res.status(401).json({ message: "Invalid token format" });
-  }
+//   const token = authHeader.split(" ")[1];
+//   if (!token) {
+//     return res.status(401).json({ message: "Invalid token format" });
+//   }
 
-  admin.auth().verifyIdToken(token)
-    .then(decodedToken => {
-      req.user = decodedToken;
-      next();
-    })
-    .catch(error => {
-      console.error("Token verification error:", error);
-      res.status(401).json({ message: "Unauthorized" });
-    });
-};
+//   admin.auth().verifyIdToken(token)
+//     .then(decodedToken => {
+//       req.user = decodedToken;
+//       next();
+//     })
+//     .catch(error => {
+//       console.error("Token verification error:", error);
+//       res.status(401).json({ message: "Unauthorized" });
+//     });
+// };
 
-export default authMiddleware;
+// export default authMiddleware;
