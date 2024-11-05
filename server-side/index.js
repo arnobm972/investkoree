20f34ea0079d3b510327ec187f26f857ceebe1e1
unknown <arnobm97@gmail.com>
2024-11-05 14:43:58 +0600

    asdasds

--- server-side/index.js
+++ server-side/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
-import userRoutes from './routes/user.js';
+// import userRoutes from './routes/user.js';
 import userSpecificRoute from './routes/userRoutes.js'
 import connectDB from './config/db.js';
 import signupRoute from '../server-side/routes/signup.js';
