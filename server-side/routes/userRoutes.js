4f7f9618da0018a6627505d930ec1c79a814ad5b
unknown <arnobm97@gmail.com>
2024-11-07 10:21:05 +0600

    asdasd

--- server-side/routes/userRoutes.js
+++ server-side/routes/userRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/profile', authToken, async (req, res) => {
     try {
-        const user = await User.findById(req.user.id).select('-password'); // Exclude password field
+        const user = await User.findById(req.user.id).select('-password'); 
         if (!user) {
             return res.status(404).json({ message: 'User not found.' });
         }
