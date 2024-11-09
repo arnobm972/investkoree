2a5375ab279d7bb48109155ae2c393e68ef375d9
unknown <arnobm97@gmail.com>
2024-11-09 21:13:21 +0600

    asdasd

--- client-side/src/Routes/PrivateFounderRoute.jsx
+++ client-side/src/Routes/PrivateFounderRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../providers/AuthProvider";
 
 const PrivateFounderRoute = ({ children }) => {
-  const { userdata } = useAuth();
+  const { userdata, loading } = useAuth();
   if (loading) {
     return <span className="loading loading-spinner loading-lg"></span>;
   }
