a51055a7417d31e0b3a27056147d528afa5dbaee
unknown <arnobm97@gmail.com>
2024-11-09 20:22:11 +0600

    asdasd

--- client-side/src/Pages/Founder/FounderLogin.jsx
+++ client-side/src/Pages/Founder/FounderLogin.jsx
@@ -34,9 +34,8 @@ const FounderLogin = () => {
 
     try {
       await foundersignIn(email, password);
-
-      toast.success("Login successful");
       navigate("/founderdashboard");
+      toast.success("Login successful");
     } catch (err) {
       toast.error(err.message || "Login error");
       setError(err.message || "Login error");
