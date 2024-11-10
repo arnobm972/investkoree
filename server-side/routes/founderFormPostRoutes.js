d6fe7fe98bbe783263d506c57548b010b12b1347
unknown <arnobm97@gmail.com>
2024-11-10 12:44:12 +0600

    aasds

--- server-side/routes/founderFormPostRoutes.js
+++ server-side/routes/founderFormPostRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createFounderPost } from '../controllers/founderFormController.js'; 
+import { authToken } from '../utils/authMiddleware.js';
 
 const router = express.Router();
 
 // Use the createFounderPost controller directly as the route handler
-router.post('/upload', createFounderPost);
+router.post('/upload', createFounderPost,authToken);
 
 export default router;
