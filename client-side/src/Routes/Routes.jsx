9070c2d2772be44493924f0cb36f0b9a72ae6ca6
unknown <arnobm97@gmail.com>
2024-11-13 11:47:36 +0600

    adadas

--- client-side/src/Routes/Routes.jsx
+++ client-side/src/Routes/Routes.jsx
@@ -18,7 +18,7 @@ import FounderPost from "../Pages/Founder/FounderPost";
 // import PrivateFounderRoute from "./PrivateFounderRoute";
 // import PrivateAdminRoute from "./PrivateAdminRoute";
 import ProjectDetail from "../Pages/ProjectDetail";
-// import Payment from "../Pages/Payment";
+import Payment from "../Pages/Payment";
 
 export const router = createBrowserRouter([
   {
@@ -94,10 +94,10 @@ export const router = createBrowserRouter([
         path: "/getfunded",
         element: <GetFunded></GetFunded>,
       },
-      // {
-      //   path: "/payment",
-      //   element: <Payment></Payment>,
-      // },
+      {
+        path: "/payment",
+        element: <Payment></Payment>,
+      },
       {
         path: "/founderdashboard",
         element: (
