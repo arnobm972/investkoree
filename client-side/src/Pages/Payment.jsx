94990242fe8358a4f85d9922cee8d1186b79c0b8
unknown <arnobm97@gmail.com>
2024-11-13 12:17:18 +0600

    adadas

--- client-side/src/Pages/Payment.jsx
+++ client-side/src/Pages/Payment.jsx
@@ -4,8 +4,8 @@ const Payment = () => {
     <div className="flex flex-col items-center p-6 bg-gray-100 min-h-screen ">
       <h1 className="text-2xl font-semibold mb-4">Payment Methods</h1>
       <p className="text-gray-600 mb-8">More methods will be added soon.</p>
-      <div className="flex items-center space-x-4 bg-white p-4 w-24 rounded-lg shadow-lg">
-        <img src={bkash} alt="bKash logo" className="h-12 w-16" />
+      <div className="flex items-center space-x-4 bg-white p-4 w-full  rounded-lg shadow-lg">
+        <img src={bkash} alt="bKash logo" className="h-12 w-20" />
         <span className="text-xl font-medium">+8801XXXXXXX</span>
       </div>
     </div>
