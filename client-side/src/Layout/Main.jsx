177586985069f11da9cd84667692ce422692ae93
unknown <arnobm97@gmail.com>
2024-11-13 12:03:20 +0600

    adadas

--- client-side/src/Layout/Main.jsx
+++ client-side/src/Layout/Main.jsx
@@ -6,7 +6,7 @@ import AuthProvider from "../providers/AuthProvider";
 
 const Main = () => {
   return (
-    <div className="roboto-regular">
+    <div className="roboto-regular bg-gray-100">
       <AuthProvider>
         <Navbar></Navbar>
         <ToastContainer></ToastContainer>
