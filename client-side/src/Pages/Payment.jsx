3edd1064ca43f0877e9bc9077ea44c0fda366bc0
unknown <arnobm97@gmail.com>
2024-11-13 12:13:40 +0600

    adadas

--- client-side/src/Pages/Payment.jsx
+++ client-side/src/Pages/Payment.jsx
@@ -1,10 +1,10 @@
 import bkash from "../assets/bkash.png";
 const Payment = () => {
   return (
-    <div className="flex flex-col items-center p-6 bg-gray-100 min-h-screen w-24">
+    <div className="flex flex-col items-center p-6 bg-gray-100 min-h-screen ">
       <h1 className="text-2xl font-semibold mb-4">Payment Methods</h1>
       <p className="text-gray-600 mb-8">More methods will be added soon.</p>
-      <div className="flex items-center space-x-4 bg-white p-4 rounded-lg shadow-lg">
+      <div className="flex items-center space-x-4 bg-white p-4 w-24 rounded-lg shadow-lg">
         <img src={bkash} alt="bKash logo" className="h-12 w-16" />
         <span className="text-xl font-medium">+8801XXXXXXX</span>
       </div>
