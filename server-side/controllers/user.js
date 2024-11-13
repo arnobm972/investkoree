48207ea84c16fbf095face393845713ba8a602f2
unknown <arnobm97@gmail.com>
2024-11-13 14:52:15 +0600

    adadas

--- server-side/controllers/user.js
+++ server-side/controllers/user.js
@@ -1,12 +1,12 @@
-// import userService from '../services/user.js';
+import userService from '../services/user.js';
 
-// async function getUsers(req, res) {
-//     try {
-//         const users = await userService.getUsers();
-//         res.json(users);
-//     } catch (error) {
-//         res.status(500).json({ message: error.message });
-//     }
-// }
+async function getUsers(req, res) {
+    try {
+        const users = await userService.getUsers();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
 
-// export default { getUsers };
+export default { getUsers };
