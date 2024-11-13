97b44971b7c777c0252b84c0b6ebd4b932c44068
unknown <arnobm97@gmail.com>
2024-11-13 12:34:53 +0600

    adadas

--- client-side/src/Pages/Payment.jsx
+++ client-side/src/Pages/Payment.jsx
@@ -1,4 +1,5 @@
 import bkash from "../assets/bkash.png";
+import city from "../assets/city.png";
 const Payment = () => {
   return (
     <div className="flex flex-col items-center p-6 bg-gray-100 min-h-screen ">
@@ -6,6 +7,10 @@ const Payment = () => {
       <p className="text-gray-600 mb-8">More methods will be added soon.</p>
       <div className="flex items-center space-x-4 bg-white p-4   rounded-lg shadow-lg">
         <img src={bkash} alt="bKash logo" className="h-12 w-20" />
+        <span className="text-xl font-medium">+8801XXXXXXX</span>
+      </div>
+      <div className="flex items-center space-x-4 bg-white p-4   rounded-lg shadow-lg">
+        <img src={city} alt="bKash logo" className="h-12 w-20" />
         <span className="text-xl font-medium">+8801XXXXXXX</span>
       </div>
     </div>
