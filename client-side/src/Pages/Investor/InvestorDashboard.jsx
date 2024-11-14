623d723c9d40503888ff05cecaf76a06ff13ca2a
unknown <arnobm97@gmail.com>
2024-11-14 11:49:24 +0600

    adsasd

--- client-side/src/Pages/Investor/InvestorDashboard.jsx
+++ client-side/src/Pages/Investor/InvestorDashboard.jsx
@@ -93,7 +93,7 @@ const InvestorDashboard = () => {
 
       try {
         const response = await fetch(
-          `${API_URL}/founderpost/projectdetail/${selectedPost._id}/posts`
+          `${API_URL}/api/${selectedPost._id}/posts`
         );
         if (!response.ok) throw new Error("Failed to fetch post details");
 
