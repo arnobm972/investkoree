bcbaa895a1edab64320dbfc7f8bdf0dd230562f1
unknown <arnobm97@gmail.com>
2024-11-18 11:12:02 +0600

    asdasd

--- client-side/src/Pages/ProjectDetail.jsx
+++ client-side/src/Pages/ProjectDetail.jsx
@@ -29,7 +29,7 @@ const ProjectDetail = () => {
   }, [id]);
 
   // If project data is not yet loaded, show a loading message
-  if (!project && !userdata) {
+  if (!project) {
     return <div>Loading...</div>;
   }
   console.log(project);
