b7cd75a7e4b770f8eaabb0abc25ba298699b19a9
unknown <arnobm97@gmail.com>
2024-11-18 12:22:02 +0600

    asdasd

--- client-side/src/shared/Navbar.jsx
+++ client-side/src/shared/Navbar.jsx
@@ -24,14 +24,17 @@ const Navbar = () => {
 
   return (
     <div className="sticky top-0 z-50 bg-white shadow-lg">
-      <div className="navbar sm:text-base xs:text-base xxs:text-base sm:font-medium xs:font-medium xxs:font-medium px-6 py-3 flex justify-between items-center">
+      <div className="navbar  px-6 py-3 flex justify-between items-center">
         <div className="flex items-center">
           <img className="h-16 w-36" src={logo} alt="logo" />
         </div>
 
         {/* Hamburger Icon for Small Screens */}
         <div className="lg:hidden block">
-          <button onClick={toggleMenu} className="text-2xl">
+          <button
+            onClick={toggleMenu}
+            className=" sm:text-base xs:text-base xxs:text-base sm:font-medium xs:font-medium xxs:font-medium text-2xl"
+          >
             {isOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
           </button>
         </div>
