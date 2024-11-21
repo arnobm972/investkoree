4edb0a820a529d0474c13ae2e95f38740989d2c4
unknown <arnobm97@gmail.com>
2024-11-21 15:54:37 +0600

    sdasd

--- client-side/src/shared/Notifications.jsx
+++ client-side/src/shared/Notifications.jsx
@@ -76,7 +76,7 @@ const Notifications = ({ API_URL, userId }) => {
                 {notifications.map((notification) => (
                   <li
                     key={notification._id}
-                    className={`p-2 rounded mr-4 text-xs text-gray-500 ${
+                    className={`p-2 rounded mr-8 text-xs text-gray-500 ${
                       notification.read
                         ? "bg-gray-100"
                         : "bg-yellow-100 font-semibold"
