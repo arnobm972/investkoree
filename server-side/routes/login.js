a6c97285554f29556630bef14d9269e62c1aa5b7
unknown <arnobm97@gmail.com>
2024-11-27 15:12:11 +0600

    asdasd

--- server-side/routes/login.js
+++ server-side/routes/login.js
@@ -1,28 +1,12 @@
 import express from "express";
 import cors from "cors";
 import loginController from "../controllers/login.js";
-import { body, validationResult } from 'express-validator';
+
 
 const router = express.Router();
 router.use(cors());
 
-// Login route with validation
-router.post(
-  "/login", 
-  [
-    body('username').trim().escape().notEmpty().withMessage('Username is required'), // Example validation
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters').escape()
-  ],
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    next(); // If validation passes, proceed to the login controller
-  },
-  loginController.login
-);
-
+router.post("/login", loginController.login);
 router.post("/refresh-token", loginController.refreshToken);
 
 export default router;
