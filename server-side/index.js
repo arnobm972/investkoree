55063881d60e32526c58d3d1a2ea8b4da328bc77
unknown <arnobm97@gmail.com>
2024-11-28 11:56:47 +0600

    asdas

--- server-side/index.js
+++ server-side/index.js
@@ -138,7 +138,7 @@ app.put('/adminpost/update/:id', authToken, upload.fields([
     // Prepare the updated 
     const updatedPost = { ...req.body };
 
-
+    
     // Handle the uploaded files
     if (req.files) {
       if (req.files.businessPicture) {
@@ -148,28 +148,28 @@ app.put('/adminpost/update/:id', authToken, upload.fields([
         }));
       }
       if (req.files.nidCopy) {
-        updatedPost.nidFile = req.files.nidCopy[0].buffer; 
+        updatedPost.nidFile = req.files.nidCopy[0].buffer; // Assuming only one file
       }
       if (req.files.tinCopy) {
-        updatedPost.tinFile = req.files.tinCopy[0].buffer; 
+        updatedPost.tinFile = req.files.tinCopy[0].buffer; // Assuming only one file
       }
       if (req.files.taxCopy) {
-        updatedPost.taxFile = req.files.taxCopy[0].buffer; 
+        updatedPost.taxFile = req.files.taxCopy[0].buffer; // Assuming only one file
       }
       if (req.files.tradeLicense) {
-        updatedPost.tradeLicenseFile = req.files.tradeLicense[0].buffer; 
+        updatedPost.tradeLicenseFile = req.files.tradeLicense[0].buffer; // Assuming only one file
       }
       if (req.files.bankStatement) {
-        updatedPost.bankStatementFile = req.files.bankStatement[0].buffer; 
+        updatedPost.bankStatementFile = req.files.bankStatement[0].buffer; // Assuming only one file
       }
       if (req.files.securityFile) {
-        updatedPost.securityFile = req.files.securityFile[0].buffer; 
+        updatedPost.securityFile = req.files.securityFile[0].buffer; // Assuming only one file
       }
       if (req.files.financialFile) {
-        updatedPost.financialFile = req.files.financialFile[0].buffer; 
+        updatedPost.financialFile = req.files.financialFile[0].buffer; // Assuming only one file
       }
-      if (req.files.videoFile) {
-        updatedPost.videoFile = req.files.videoFile[0].buffer; 
+      if (req.files.video) {
+        updatedPost.video = req.files.video[0].buffer; // Assuming only one file
       }
     }
 
