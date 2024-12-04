324bead10b45d210684537ac00984810c9547c18
unknown <arnobm97@gmail.com>
2024-12-04 11:54:59 +0600

    sadasd

--- server-side/models/userModel.js
+++ server-side/models/userModel.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User', index: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', index: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, required: true, enum: ['investor', 'founder','admin'] }, 
